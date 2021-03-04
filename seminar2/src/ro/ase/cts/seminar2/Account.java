a11482085a02c8e9d8f4017f15c0daa6245c462a
Filip1991 <filip.victor@yahoo.com>
2021-03-04 08:23:14 +0200

    before break

---
+++ seminar2/src/ro/ase/cts/seminar2/Account.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.seminar2;
+
+public abstract class Account {
+	
+	public abstract void deposit();
+	public abstract void withdraw();
+	public abstract void transfer();
+	public abstract double getBalance();
+
+}
